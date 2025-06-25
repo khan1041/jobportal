4a1d200259d758e7f526afcacb4fe595a8696704
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-06-25 10:32:55 +0600

    Update Usercontoler.js

--- controller/Usercontoler.js
+++ controller/Usercontoler.js
@@ -133,7 +133,7 @@ export const Login=async (req, res) => {
             profile: user.profile
         }
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000,}).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000}).json({
             message: `Welcome back ${user.fullname}`,
             user,
             success: true
