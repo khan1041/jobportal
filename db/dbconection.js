e78228f2d5198bb5e13c9018903ae85b5ea9e7b6
Md.Shimul Khan <shimulssk.121@gmail.com>
2025-06-25 11:23:34 +0600

    Update dbconection.js

--- db/dbconection.js
+++ db/dbconection.js
@@ -7,7 +7,7 @@ const uri=process.env.url
 const conectedDb=async ()=>{
 
     try {
-        await mongoose.connect("mongodb+srv://skshimul79977007:jVsznoSExXGEY71U@cluster0.czfbnjj.mongodb.net/jobportal")
+        await mongoose.connect("mongodb+srv://skshimul79977007:tiBPI7LUbBnOwdoE@cluster0.cwfmg5j.mongodb.net/jobportal2")
         console.log("conceted done to db")
     } catch (error) {
         console.error("database connected faill")
